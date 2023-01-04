fc42c579f194c6c0fc462bcf14071451e13b70f9
Devi Prasad Palakollu <prasad20697@gmail.com>
2023-01-04 22:43:59 +0530

    server init

---
+++ server/src/index.ts
@@ -0,0 +1,82 @@
+import express from "express";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+import http from "http";
+import logger from "morgan";
+import cors from "cors";
+import { router as indexRoute } from "./routes/index";
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(logger("dev"));
+app.use(cors({ origin: "*" }));
+
+const port = normalizePort(process.env.PORT || "5000");
+app.set("port", port);
+
+app.use("/api/v1", indexRoute);
+
+const server = http.createServer(app);
+
+// Database connection.
+mongoose.set("strictQuery", false);
+mongoose
+	.connect(
+		"mongodb+srv://admin:OnhZ3KWS9bo4UsTL@quickcards.e7fcc3x.mongodb.net/?retryWrites=true&w=majority"
+	)
+	.then(() => {
+		server.listen(port);
+		server.on("error", onError);
+	})
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
+
+mongoose.connection
+	.once("open", () =>
+		console.log(
+			`Database connected. Listening on port: ${port}. \nWaiting for requests...`
+		)
+	)
+	.on("error", onError);
+
+function normalizePort(val: string) {
+	const port = parseInt(val, 10);
+
+	if (isNaN(port)) {
+		// named pipe
+		return val;
+	}
+
+	if (port >= 0) {
+		return port;
+	}
+	return false;
+}
+
+function onError(error: any) {
+	if (error.syscall !== "listen") {
+		throw error;
+	}
+
+	const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+	// handle specific listen errors with friendly messages
+	switch (error.code) {
+		case "EACCES":
+			console.error(bind + " requires elevated privileges");
+			process.exit(1);
+			break;
+		case "EADDRINUSE":
+			console.error(bind + " is already in use");
+			process.exit(1);
+			break;
+		default:
+			throw error;
+	}
+}
