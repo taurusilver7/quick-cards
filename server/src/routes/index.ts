d6101366356746ce0a5b613a8d68b439aa6a1e69
Devi Prasad Palakollu <prasad20697@gmail.com>
2023-01-05 11:23:55 +0530

    api routes adjusted

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,25 @@
 import express from "express";
+import {
+	createCard,
+	createDeck,
+	deleteCard,
+	deletetDeck,
+	getDeck,
+	getDecks,
+} from "../controller/deck";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-	res.send("Hello World");
-});
+// router.get("/", (req, res) => {
+// 	res.send("Hello World");
+// });
+
+router.get("/", getDecks);
+router.post("/", createDeck);
+router.delete("/:deckId", deletetDeck);
+router.get("/:deckId", getDeck);
+
+router.post("/:deckId/card", createCard);
+router.delete("/:deckId/card/:index", deleteCard);
 
 export { router };
