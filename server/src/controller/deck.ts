9abea861854bc5d9f03ac31c5a282ebf134abf1d
Devi Prasad Palakollu <prasad20697@gmail.com>
2023-01-05 22:03:55 +0530

    models & controller modified

--- server/src/controller/deck.ts
+++ server/src/controller/deck.ts
@@ -11,19 +11,73 @@ export async function getDecks(req: Request, res: Response) {
 	}
 }
 
-export async function getDeck(req: Request, res: Response) {}
+export async function getDeck(req: Request, res: Response) {
+	const { deckId } = req.params;
+
+	try {
+		const deck = await Deck.findById(deckId);
+		res.status(200).json(deck);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json(error);
+	}
+}
 
 export async function createDeck(req: Request, res: Response) {
 	const title = req.body.title;
-	const createdDeck = new Deck({
-		title,
-	});
-	const newDeck = await createdDeck.save();
-	res.status(201).json(newDeck);
+
+	if (title) {
+		const createdDeck = new Deck({
+			title,
+		});
+		const newDeck = await createdDeck.save();
+		res.status(201).json(newDeck);
+	} else {
+		res.status(400).json({ message: "Deck needs a title to create." });
+	}
 }
 
-export async function deletetDeck(req: Request, res: Response) {}
+export async function deletetDeck(req: Request, res: Response) {
+	const deckId = req.params.deckId;
 
-export async function createCard(req: Request, res: Response) {}
+	try {
+		const deck = await Deck.findByIdAndDelete(deckId);
+		res.status(200).json("Deck deleted Successfully");
+	} catch (error) {
+		console.error(error);
+		res.status(500).json(error);
+	}
+}
 
-export async function deleteCard(req: Request, res: Response) {}
+export async function createCard(req: Request, res: Response) {
+	const { deckId } = req.params;
+	try {
+		const deck = await Deck.findById(deckId);
+		if (!deck) {
+			return res.status(400).json("Deck doesn't exist");
+		}
+		const { text } = req.body;
+		deck.cards.push(text);
+		await deck.save();
+		res.status(200).json(deck);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json(error);
+	}
+}
+
+export async function deleteCard(req: Request, res: Response) {
+	const { deckId, index } = req.params;
+	try {
+		const deck = await Deck.findById(deckId);
+		if (!deck) {
+			return res.status(400).json("Deck doesn't exist");
+		}
+		deck.cards.splice(parseInt(index), 1);
+		await deck.save();
+		res.status(200).json(deck);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json(error);
+	}
+}
--- server/src/models/Deck.ts
+++ server/src/models/Deck.ts
@@ -3,10 +3,13 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.Types.ObjectId;
 
-const DeckSchema = new Schema({
-   title: String,
-})
+const DeckSchema = new Schema(
+	{
+		title: String,
+		cards: [String],
+	},
+	{ timestamps: true }
+);
 
 const Deck = mongoose.model("Deck", DeckSchema);
 export default Deck;
-
